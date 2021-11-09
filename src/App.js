c2736b2e707b1f5dda6acdd41229cdc55b647ccc
SeobisBack <seobisback@gmail.com>
2021-11-09 16:04:28 +0900

    Use Coin API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState();
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);
+    });
+    
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      <hr/>
+      {loading ? <strong>Loading...</strong> : 
+      <select>
+        {coins.map((coin) => (
+            <option>{coin.name} ({coin.symbol}) : ({coin.quotes.USD.price})USD</option>
+        ))}
+      </select>}
+      
     </div>
   );
 }
